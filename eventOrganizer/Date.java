353ee031a6111a146a91dd6bcee244662d41f735
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-09-19 17:36:02 -0400

    Java docs

--- eventOrganizer/Date.java
+++ eventOrganizer/Date.java
@@ -1,7 +1,15 @@
 package eventOrganizer;
-import java.util.Calendar;
 
+
+/**
+ * @Author Seifeldeen Mohamed
+ * Represents a date in the MM/DD/YYYY format and provides methods for date validation.
+ */
 public class Date implements Comparable<Date> {
+    /**
+     *
+     *
+     */
     public static final int QUADRENNIAL = 4;
     public static final int CENTENNIAL = 100;
     public static final int QUATERCENTENNIAL = 400;
@@ -10,18 +18,29 @@ public class Date implements Comparable<Date> {
     private int month;
     private int day;
 
+    /**
+     * A Constructor that has a new Date object with the specified year, month, and day.
+     *
+     * @param year (YYYY)
+     * @param month The month (1-12)
+     * @param day The day of the month (1-31).
+     */
     public Date(int year, int month, int day){
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    public boolean isValid(){
 
+    /**
+     * A public method that checks if the date is a valid calendar date.
+     *
+     * @return True if the date is valid; otherwise, false.
+     */
+    public boolean isValid(){
         if (month < 1 || month > 12){
             return false;
         }
-
         int [] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         if(isLeapYear(year)){
@@ -29,8 +48,15 @@ public class Date implements Comparable<Date> {
         };
 
         return day >= 1 && day <= daysInMonth[month];
-
     }
+
+    /**
+     * Compares this date to another date.
+     *
+     * @param otherDate The other date to compare to.
+     * @return A negative integer if this date is before the other date,
+     *         zero if they are equal, and a positive integer if this date is after the other date.
+     */
     public int compareTo(Date otherDate) {
         if (this.year != otherDate.year) {
             return Integer.compare(this.year, otherDate.year);
@@ -43,7 +69,22 @@ public class Date implements Comparable<Date> {
         return Integer.compare(this.day, otherDate.day);
     }
 
+
+    /**
+     * Checks if a given year is a leap year or not
+     *
+     * @param year The int year to check for leap year status.
+     * @return True if the year is a leap year; otherwise, false.
+     */
     private boolean isLeapYear(int year){
+        /**
+         * To determine whether a year is a leap year according to the Gregorian calendar:
+         * 1) If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+         * 2) If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+         * 3) If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+         * 4) The year is a leap year.
+         * 5) The year is not a leap year.
+         */
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             return true;
         }
@@ -69,6 +110,3 @@ public class Date implements Comparable<Date> {
         System.out.println("Is leapYearDate valid? " + leapYearDate.isValid()); // Should be true
     }
 };
-
-
-
--- eventOrganizer/Location.java
+++ eventOrganizer/Location.java
@@ -1,23 +1,37 @@
 package eventOrganizer;
+
+
+/**
+ * enum class for campus locations using acronyms and provides the full name and campus location for each campus acronym
+ *
+ */
 public enum Location {
     HILL114("Hill Center, Busch"),
     ARC103("Allison Road Classroom, Busch"),
     BE_AUD("Beck Hall, Livingston"),
-    TIL232("Tillettt Hall, Livingston"),
+    TIL232("Tillet Hall, Livingston"),
     AB2225("Academic Building, College Avenue");
 
     private final String description;
 
+    /**
+     * Constructs a Location enum value with the specified description.
+     *
+     * @param description The description of the campus location.
+     */
     Location(String description){
         this.description = description;
-    }
+    };
 
+    /**
+     * Returns the description of the location.
+     */
     public String getDescription(){
         return description;
-    }
-
+    };
 
     public static void main (String[] args){
         System.out.println(Location.HILL114 + " - " + Location.HILL114.getDescription());
         System.out.println(Location.ARC103 + " - " + Location.ARC103.getDescription());
-    }
+    };
+};
