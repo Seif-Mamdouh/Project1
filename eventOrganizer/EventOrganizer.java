7dc12b90c50c7d4c8a78715bf2a13757935512cb
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-09-28 18:25:38 -0400

    small bug fix

--- eventOrganizer/EventOrganizer.java
+++ eventOrganizer/EventOrganizer.java
@@ -218,7 +218,7 @@ public class EventOrganizer {
                 displayCalendar();
                 break;
             case "PE":
-                displayCalendar();
+                displayCalendarByDate();
                 break;
             case "PC":
                 displayCalendarByCampus();
