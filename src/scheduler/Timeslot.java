2e2a1af6d19c02295d1c865dc15286988f53b40a
MichaelMuz <20muzm01@gmail.com>
2023-09-30 21:09:28 -0400

    implement time adding logic

--- src/scheduler/Timeslot.java
+++ src/scheduler/Timeslot.java
@@ -1,22 +1,35 @@
 package scheduler;
 
+import java.util.Calendar;
+
 /**
  * Enum class to represent all departments.
  *
  * @author Michael Muzafarov
  */
 public enum Timeslot {
-    MORNING("Morning"), AFTERNOON("Afternoon"), EVENING("Evening"),
+    MORNING("Morning", 10, 30, Calendar.AM),
+    AFTERNOON("Afternoon", 2, 0, Calendar.PM),
+    EVENING("Evening", 6, 30, Calendar.PM),
     ;
 
     private final String stringRepresentation;
+    private final int HOUR, MINUTE, AM_PM;
+
 
     /**
      * Builds enum with given string representation.
      *
      * @param stringRepresentation enum as string
+     * @param hour                 hour of the day time slot starts
+     * @param minute               minute time slot starts
+     * @param amPm                 represents if time is am or pm,
+     *                             corresponds to Calendar.AM and Calendar.PM
      */
-    Timeslot(String stringRepresentation) {
+    Timeslot(String stringRepresentation, int hour, int minute, int amPm) {
+        this.HOUR = hour;
+        this.MINUTE = minute;
+        this.AM_PM = amPm;
         this.stringRepresentation = stringRepresentation;
     }
 
@@ -29,6 +42,64 @@ public enum Timeslot {
     @Override
     public String toString() {
         return this.stringRepresentation;
+    }
+
+    /**
+     * return the time after a duration
+     *
+     * @param duration the number of minutes elapsed since start time
+     * @return end time as string
+     */
+    public String timeAfterDuration(int duration) {
+        Calendar calendarForTime = Calendar.getInstance();
+        calendarForTime.set(Calendar.HOUR, this.HOUR);
+        calendarForTime.set(Calendar.MINUTE, this.MINUTE);
+        calendarForTime.set(Calendar.AM_PM, this.AM_PM);
+
+        calendarForTime.add(Calendar.MINUTE, duration);
+        int hour = calendarForTime.get(Calendar.HOUR);
+
+        int hour12AccordingToCalendar = 0;
+        if (hour == hour12AccordingToCalendar) {
+            hour = 12;
+        }
+        int minute = calendarForTime.get(Calendar.MINUTE);
+
+        String StringFormatWith2DigitPadding = "%02d";
+        return Integer.toString(hour) + ":" +
+               String.format(StringFormatWith2DigitPadding, minute) +
+               (calendarForTime.get(Calendar.AM_PM) == Calendar.AM ? "AM" : "PM"
+               );
+    }
+
+    /**
+     * Unit tests for the timeAfterDuration method
+     *
+     * @param args unused, does not take command line arguments
+     */
+    public static void main(String[] args) {
+        assert (Timeslot.MORNING.timeAfterDuration(5).equals("10:35AM"));
+        assert (Timeslot.MORNING.timeAfterDuration(30).equals("11:00AM"));
+        assert (Timeslot.MORNING.timeAfterDuration(60).equals("11:30AM"));
+        assert (Timeslot.MORNING.timeAfterDuration(89).equals("11:59AM"));
+        assert (Timeslot.MORNING.timeAfterDuration(90).equals("12:00PM"));
+        assert (Timeslot.MORNING.timeAfterDuration(120).equals("12:30PM"));
+
+        assert (Timeslot.AFTERNOON.timeAfterDuration(5).equals("2:05PM"));
+        assert (Timeslot.AFTERNOON.timeAfterDuration(30).equals("2:30PM"));
+        assert (Timeslot.AFTERNOON.timeAfterDuration(60).equals("3:00PM"));
+        assert (Timeslot.AFTERNOON.timeAfterDuration(89).equals("3:29PM"));
+        assert (Timeslot.AFTERNOON.timeAfterDuration(90).equals("3:30PM"));
+        assert (Timeslot.AFTERNOON.timeAfterDuration(120).equals("4:00PM"));
+
+        assert (Timeslot.EVENING.timeAfterDuration(5).equals("6:35PM"));
+        assert (Timeslot.EVENING.timeAfterDuration(30).equals("7:00PM"));
+        assert (Timeslot.EVENING.timeAfterDuration(60).equals("7:30PM"));
+        assert (Timeslot.EVENING.timeAfterDuration(89).equals("7:59PM"));
+        assert (Timeslot.EVENING.timeAfterDuration(90).equals("8:00PM"));
+        assert (Timeslot.EVENING.timeAfterDuration(120).equals("8:30PM"));
+
+
     }
 
 }
